3d562ac8d0e4ba46532f0ac2a4fca470412bc294
Шепыч <syndicating@yandex.ru>
2022-07-29 23:34:44 +0300

    Первая инициализация

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Чеккер</title>
+</head>
+<body>
+    <main>
+        <h1>Чеккер</h1>
+        <form>
+            <select name="section" id="">
+                <option value="">Голова</option>
+                <option value="">Ноги</option>
+                <option value="">Руки</option>
+                <option value="">Спина</option>
+                <option value="">Кожа</option>
+            </select>
+            <h4>Вопрос для диагноза</h4>
+            <div>
+                <input type="submit" value="Да">
+                <input type="submit" value="нет">
+            </div>
+        </form>
+    </main>
+</body>
+</html>
